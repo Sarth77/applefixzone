e92774fa0195717a70e8d83bf79dc05e5449ba93
Sahil Tomar <sahil3112109695@gmail.com>
2023-05-30 19:47:34 +0530

    select none

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -31,18 +31,18 @@ const Search = () => {
               className="focus:outline-none focus:border-none p-[5px] rounded-full ml-2 w-full"
             />
           </div>
-          <div className="icon flex items-center text-xl justify-end">
-            <div className="flex items-center gap-x-6">
+          <div className="icon flex items-center text-xl justify-end select-none">
+            <div className="flex items-center gap-x-6 select-none">
               <Link to="/login">
-                <div className="bg-[#f6f9fc] h-10 w-10 rounded-full flex justify-center items-center drop-shadow-md hover:scale-110 cursor-pointer">
-                  <i className="fa fa-user icon-circle"></i>
+                <div className="bg-[#f6f9fc] h-10 w-10 rounded-full flex justify-center items-center drop-shadow-md hover:scale-110 cursor-pointer select-none">
+                  <i className="fa fa-user icon-circle select-none"></i>
                 </div>
               </Link>
               <div className="bg-[#f6f9fc] h-10 w-10 rounded-full flex justify-center items-center drop-shadow-md hover:scale-110">
-                <div className="relative">
+                <div className="relative select-none">
                   <Link to="/cart">
-                    <i className="fa fa-shopping-bag icon icon-circle"></i>
-                    <span className="absolute flex top-[-20%] right-[-35%] w-[15px] h-[15px] rounded-full items-center justify-center text-[0.8rem] font-bold text-white bg-black">
+                    <i className="fa fa-shopping-bag icon icon-circle select-none"></i>
+                    <span className="absolute flex top-[-20%] right-[-35%] w-[15px] h-[15px] rounded-full items-center justify-center text-[0.8rem] font-bold text-white bg-black select-none">
                       0
                     </span>
                   </Link>
