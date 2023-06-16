256b73c881506557984f298c61b96a2cc63fb48f
Sahil Tomar <sahil3112109695@gmail.com>
2023-06-16 22:02:26 +0530

    cartbgcorrect

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -32,7 +32,7 @@ const Cart = () => {
   return (
     <>
       <div className="snap-start max-w-[90%] m-auto py-12">
-        <div className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:border-gray-700">
           <div className="flex items-center justify-between mb-4">
             <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
               Shopping Cart
